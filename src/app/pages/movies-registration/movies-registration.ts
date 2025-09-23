d02f2ec75e08dd271ab7a57abc56210b9404f597
Kirink212 <lfernandotexbicalho@hotmail.com>
2025-09-23 17:33:56 -0300

    Refactor movie form submission events

--- src/app/pages/movies-registration/movies-registration.ts
+++ src/app/pages/movies-registration/movies-registration.ts
@@ -1,5 +1,10 @@
-import { Component } from '@angular/core';
-import { MovieForm } from '../../components/movie-form/movie-form';
+import { Component, ViewChild, inject } from '@angular/core';
+import { switchMap } from 'rxjs';
+
+import { MovieForm, MovieFormCreateEvent } from '../../components/movie-form/movie-form';
+import { MoviesService } from '../../services/movies.service';
+import { Movie } from '../../models/movie';
+import { MatSnackBar } from '@angular/material/snack-bar';
 
 @Component({
   selector: 'app-movies-registration',
@@ -8,5 +13,44 @@ import { MovieForm } from '../../components/movie-form/movie-form';
   styleUrl: './movies-registration.scss'
 })
 export class MoviesRegistration {
+  @ViewChild(MovieForm) movieForm?: MovieForm;
+
+  private readonly moviesService = inject(MoviesService);
+  private readonly snackBar = inject(MatSnackBar);
+
+  handleCreateMovie(event: MovieFormCreateEvent) {
+    const { formValue, file } = event;
+
+    this.moviesService
+      .uploadImage(file)
+      .pipe(
+        switchMap(({ imageUrl }) => {
+          const payload: Omit<Movie, 'id'> = {
+            ...formValue,
+            imageLink: imageUrl
+          };
+
+          return this.moviesService.create(payload);
+        })
+      )
+      .subscribe({
+        next: () => {
+          this.movieForm?.resetAfterCreate();
+          this.snackBar.open('Filme adicionado com sucesso!', 'Fechar', {
+            horizontalPosition: 'end',
+            verticalPosition: 'top',
+            duration: 3000
+          });
+        },
+        error: (error) => {
+          console.error(error);
+          this.snackBar.open('Não foi possível adicionar o filme.', 'Fechar', {
+            horizontalPosition: 'end',
+            verticalPosition: 'top',
+            duration: 3000
+          });
+        }
+      });
+  }
 
 }
