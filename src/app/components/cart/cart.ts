cce0be3c2a1999ee08885f9c0d46de5c338c21a1
Kirink212 <lfernandotexbicalho@hotmail.com>
2025-09-26 18:07:24 -0300

    feat: add stripe checkout modal to cart

--- src/app/components/cart/cart.ts
+++ src/app/components/cart/cart.ts
@@ -3,13 +3,17 @@ import { CartItem } from '../../models/cart-item';
 import { CartService } from '../../services/cart.service';
 import { MatIcon } from '@angular/material/icon';
 import { MatListModule } from '@angular/material/list';
-import { MatIconButton } from '@angular/material/button';
+import { MatButtonModule, MatIconButton } from '@angular/material/button';
 import { MatSnackBar } from '@angular/material/snack-bar';
 import { CurrencyPipe } from '@angular/common';
+import { MatDialog, MatDialogModule } from '@angular/material/dialog';
+import { MatDividerModule } from '@angular/material/divider';
+
+import { PaymentModal } from '../payment-modal/payment-modal';
 
 @Component({
   selector: 'app-cart',
-  imports: [ MatIcon, MatIconButton,  MatListModule, CurrencyPipe ],
+  imports: [ MatIcon, MatIconButton, MatButtonModule, MatDialogModule, MatListModule, MatDividerModule, CurrencyPipe ],
   templateUrl: './cart.html',
   styleUrl: './cart.scss'
 })
@@ -17,7 +21,11 @@ export class Cart implements OnInit {
   cartArray: Array<CartItem> = [];
   getTotalPrice: Signal<number> = signal<number>(0);
 
-  constructor(private cartService: CartService, private snackBar: MatSnackBar) {
+  constructor(
+    private cartService: CartService,
+    private snackBar: MatSnackBar,
+    private dialog: MatDialog
+  ) {
     this.getTotalPrice = this.cartService.getTotalPrice;
   }
 
@@ -40,5 +48,24 @@ export class Cart implements OnInit {
 
   removeItem(item: CartItem) {
     this.cartService.removeItem(item.movie);
+  }
+
+  openPaymentModal() {
+    if (!this.cartArray.length) {
+      this.snackBar.open('Adicione itens ao carrinho antes de finalizar a compra.', 'Fechar', {
+        horizontalPosition: 'end',
+        verticalPosition: 'top',
+        duration: 3000
+      });
+      return;
+    }
+
+    this.dialog.open(PaymentModal, {
+      data: {
+        total: this.getTotalPrice(),
+        items: this.cartArray
+      },
+      width: '500px'
+    });
   }
 }
